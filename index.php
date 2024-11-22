f9e2f9ef71349dfeb000b5adafb18d16a80743fc
oscar <100007810.joan23@fje.edu>
2024-11-22 17:17:29 +0100

    inicio

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
-    <link rel="stylesheet" type="text/css" href="./../CSS/styles.css">
+    <link rel="stylesheet" type="text/css" href="./CSS/styles.css">
 </head>
 <body>
     <header>
